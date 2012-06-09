7b46f09433ae593827bd46484d1710c885843c53
imperugo <imperugo@gmail.com>
2012-06-09 10:22:58 +0200

    added file structure for date js localization

---
+++ Javascript Localization/imperugo.jsLocalization/Controllers/LocalizationController.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using System.Web.Mvc;
+
+namespace imperugo.jsLocalization.Controllers{
+	public class LocalizationController : Controller{
+		
+		[AcceptVerbs(HttpVerbs.Get)]
+		public ActionResult DateFormat(string id){
+			if(string.IsNullOrEmpty(id)){
+				id = "en-US";
+			}
+
+			var culture = new CultureInfo(id);
+
+			return Json(culture.DateTimeFormat,JsonRequestBehavior.AllowGet);
+		}
+	}
+}
